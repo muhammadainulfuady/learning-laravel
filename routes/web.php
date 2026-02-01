4f1c40684b0753ce48283c3989eb2bb8783c1cc4
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-02-01 18:17:49 +0700

    final alhamdulillah

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DashboardPostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AdminCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -73,4 +74,5 @@ Route::get("/dashboard", function () {
 // resource
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
 ?>
